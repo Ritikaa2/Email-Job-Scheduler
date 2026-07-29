b1dfb60a8d6ea5f435e1e0d0ed6dabb8b216cff8
Ritika <148696166+Ritikaa2@users.noreply.github.com>
2026-07-30 01:00:39 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,7 +16,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: config.frontendUrl,
+    frontendUrl: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
